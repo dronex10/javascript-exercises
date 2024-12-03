3fcf1256f4469ed52d7722dd12cd4b6c5c752e90
dronex10 <152669289+dronex10@users.noreply.github.com>
2024-12-03 07:21:31 -0500

    complete javascript for exercises 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elemsToRemove) {
+    let newArray = inputArray.filter((elem) => {
+	return !elemsToRemove.includes(elem);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(valueOne, valueTwo) {
+    let higherValue;
+    if (valueOne < 0 ||
+	valueTwo < 0 ||
+        !Number.isInteger(valueOne) ||
+        !Number.isInteger(valueTwo)) {
+	return 'ERROR';
+    } else if (valueOne === valueTwo) {
+	return valueOne;
+    } else if (valueOne > valueTwo) {
+	higherValue = valueOne;
+    } else {
+	higherValue = valueTwo;
+    }
+    return (higherValue*(higherValue+1)/2);
 };
 
 // Do not edit below this line
