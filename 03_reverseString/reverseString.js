e2cfefa432890646265b46c0b732c3bc83006d84
dronex10 <152669289+dronex10@users.noreply.github.com>
2024-12-03 06:21:49 -0500

    created functions to complete exercises 01-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let inputStringArray = inputString.split("");
+    let newStringArray = [];
+    for (i = inputStringArray.length-1; i >= 0; i--) {
+	newStringArray.push(inputStringArray[i]);
+    }
+    return newStringArray.join("");
 };
 
 // Do not edit below this line
