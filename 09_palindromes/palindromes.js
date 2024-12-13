80f4fae3defd7b7cb18516d9942068c098e92358
dronex10 <152669289+dronex10@users.noreply.github.com>
2024-12-13 15:36:53 -0500

    implemented palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let comparator = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`\s{|}~]/g, '').toLowerCase();
+  const reversed = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`\s{|}~]/g, '').toLowerCase().split('').reverse().join('');
+  return (comparator === reversed);
 };
 
 // Do not edit below this line
