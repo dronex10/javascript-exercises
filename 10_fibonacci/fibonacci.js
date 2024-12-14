6dbfbd6cb74a39d185ecb932d302235983316cf5
dronex10 <152669289+dronex10@users.noreply.github.com>
2024-12-14 13:32:37 -0500

    impleneted fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) {
+    return "OOPS"
+  }
+  num = Number(num);
+  let curr = 1;
+  let last = 0;
+  for (let i = 0; i < num; i++) {
+    let newLast = curr;
+    curr += last;
+    last = newLast;
+  }
+  return last;
 };
 
 // Do not edit below this line
