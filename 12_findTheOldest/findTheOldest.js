25ee9929b82ae5ec264bac47e7cedd35d37d087b
dronex10 <152669289+dronex10@users.noreply.github.com>
2024-12-14 13:58:25 -0500

    implemented findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sortedAges = people.sort((a, b) => {
+      a.yearOfDeath = a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear();
+      b.yearOfDeath = b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear();
+      const aAge = a.yearOfDeath - a.yearOfBirth;
+      const bAge = b.yearOfDeath - b.yearOfBirth;
+      return aAge - bAge;
+    });
+    return sortedAges[sortedAges.length - 1];
 };
 
 // Do not edit below this line
